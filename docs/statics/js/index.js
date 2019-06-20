e54d1f26c188816cf06e5aeb51fdd32da318fc98
jhonnatan <jjhoncv@gmail.com>
2019-06-20 00:39:07 -0500

    feat(build): docs

--- docs/statics/js/index.js
+++ docs/statics/js/index.js
@@ -1,1 +1,30 @@
-console.log('here!');
+// functions for scroll sections
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+}; // functions menu scrollTop
+
+
+$(".js-menu li > a").on("click", e => {
+  e.preventDefault();
+  const ele = $(e.target);
+  const li = ele.parent();
+  $(".js-menu li").removeClass('active');
+  li.addClass('active');
+  const section = ele.attr("href");
+  $(section).scrollView();
+  $(".wrapper-menu").hide();
+  $("nav").removeClass("open");
+});
+$(".menu-icon").on("click", function () {
+  if ($(this).parent().hasClass('open')) {
+    $(".wrapper-menu").hide();
+    $("nav").removeClass("open");
+  } else {
+    $(".wrapper-menu").show();
+    $("nav").addClass("open");
+  }
+});
